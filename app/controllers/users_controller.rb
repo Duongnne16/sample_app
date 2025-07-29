382c12a83e4284269e58e29e4b87a40b3d353d59
DaiDuong <duongnguyen16coha@gmail.com>
2025-07-29 09:42:36 +0000

    chapter_10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_action :load_user, only: :show
-
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :load_user, only: %i(show edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
   # GET /:locale/users/:id
   def show; end
 
@@ -11,21 +13,55 @@ class UsersController < ApplicationController
 
   # POST /:locale/signup
   def create
-    @user = User.new user_params
+    @user = User.new(user_params)
 
     if @user.save
       reset_session
       log_in @user
-      flash[:success] = t(".success")
+      flash[:success] = t(".success") # users.create.success
       redirect_to @user, status: :see_other
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  # GET /:locale/users/:id/edit
+  def edit; end
+
+  # PATCH/PUT /:locale/users/:id
+  def update
+    if @user.update(user_params)
+      flash[:success] = t(".success")
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @pagy, @users = pagy(User.ordered)
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t(".success") # users.destroy.success
+    else
+      flash[:error] = t(".error") # users.destroy.error
+    end
+    redirect_to users_path
+  end
+
   private
+
+  def admin_user
+    return if current_user&.admin?
+
+    flash[:danger] = t("users.not_authorized")
+    redirect_to root_path
+  end
+
   def load_user
-    @user = User.find_by id: params[:id]
+    @user = User.find_by(id: params[:id])
     return if @user
 
     flash[:danger] = t("users.not_found")
@@ -34,5 +70,20 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(User::USER_PARAMS)
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t("users.login_required")
+    redirect_to login_path, status: :see_other
+  end
+
+  def correct_user
+    return if current_user?(@user)
+
+    flash[:error] = t("users.not_authorized")
+    redirect_to root_path, status: :see_other
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
   # Returns the Gravatar for the given user.
-  def gravatar_for user
-    gravatar_id = Digest::MD5.hexdigest user.email.downcase
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag gravatar_url, alt: user.name, class: "gravatar"
+  def gravatar_for user, options = {size: 80}
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    size = options[:size]
+    default_url = "identicon" # fallback if user does not have a gravatar
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{default_url}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
   def gender_text user
@@ -12,5 +14,9 @@ module UsersHelper
 
   def gender_options
     User.genders.keys.map {|g| [I18n.t("users.genders.#{g}"), g]}
+  end
+
+  def can_destroy_user? user
+    current_user&.admin? && !current_user?(user)
   end
 end
